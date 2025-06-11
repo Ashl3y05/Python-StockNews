0b7e8f44d3e80df29feeea2ffb457ff728941d2d
Ashley <panelo.ashley@gmail.com>
2025-06-11 21:49:35 +0800

    Will now send a news report from previous day via whatsapp

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+from twilio.rest import Client
 import requests
 
 STOCK = "TSLA"
@@ -8,6 +9,8 @@ alphavantage_endpoint = "https://www.alphavantage.co/query"
 alphavantage_api_key =  os.environ["ALPHAVANTAGE_API_KEY"]
 newsapi_endpoint = "https://newsapi.org/v2/everything"
 newsapi_api_key = os.environ["NEWSAPI_API_KEY"]
+account_sid =os.environ["TWILIO_SID"]
+auth_token = os.environ["TWILIO_TOKEN"]
 
 av_parameters = {
     "function":"TIME_SERIES_DAILY",
@@ -25,8 +28,14 @@ def percentage_difference(value1, value2):
     except ZeroDivisionError:
         return "Error! Cannot Divide Zero"
 
-## STEP 1: Use https://www.alphavantage.co
-# When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
+def price_movement(yesterday, before):
+    if yesterday > before:
+        return "🔺"
+    elif before > yesterday:
+        return "🔻"
+    else:
+        return "="
+
 
 response = requests.get(url=alphavantage_endpoint,params=av_parameters)
 response.raise_for_status()
@@ -43,8 +52,7 @@ print(f"\nDifference: {percentage_difference(yesterday_data, day_before)}%")
 if percentage_difference(yesterday_data,day_before) >= 5:
     print("Get News")
 
-## STEP 2: Use https://newsapi.org
-# Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME. 
+
 n_api_parameters = {
     "q":COMPANY_NAME,
     "from":stock_dates[0],
@@ -55,19 +63,20 @@ news_response = requests.get(url=newsapi_endpoint,params=n_api_parameters)
 news_response.raise_for_status()
 news_data = news_response.json()
 print(news_data)
-## STEP 3: Use https://www.twilio.com
-# Send a seperate message with the percentage change and each article's title and description to your phone number. 
 
 
-#Optional: Format the SMS message like this: 
-"""
-TSLA: 🔺2%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-or
-"TSLA: 🔻5%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-"""
+header = f"TSLA: {price_movement(yesterday_data, day_before)}{percentage_difference(yesterday_data, day_before)}"
+headline = f"Headline: {news_data["articles"][0]["title"]}"
+detail = f"Brief: {news_data["articles"][0]["description"]}"
+
+client = Client(account_sid, auth_token)
+
+message = client.messages.create(
+    from_='whatsapp:+14155238886',
+    body=f"{header}\n{headline}\n{detail}",
+    to='whatsapp:+639993543575'
+)
+
+print(message.status)
 
 
